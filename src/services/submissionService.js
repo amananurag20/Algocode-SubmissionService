71299e5e78670f0caf01fd165ab2013ec1e9acee
aman anurag <amananurag.20@gmail.com>
2025-03-10 12:22:24 +0530

    added: error handler and called in index.js

--- src/services/submissionService.js
+++ src/services/submissionService.js
@@ -1,24 +1,29 @@
-const SubmissionProducer = require('../producers/submissionQueueProducer');
+const SubmissionCreationError = require("../errors/submissionCreationError");
+const SubmissionProducer = require("../producers/submissionQueueProducer");
 class SubmissionService {
-    constructor(submissionRepository) {
-        // inject here
-        this.submissionRepository = submissionRepository;
-    }
+  constructor(submissionRepository) {
+    // inject here
+    this.submissionRepository = submissionRepository;
+  }
 
-    async pingCheck() {
-        return 'pong'
-    }
+  async pingCheck() {
+    return "pong";
+  }
 
-    async addSubmission(submissionPayload) {
-        const submission = await this.submissionRepository.createSubmission(submissionPayload);
-        if(!submission) {
-            // TODO: Add error handling here
-            throw {messgae: "Not able to create submission"};
-        }
-        console.log(submission);
-        const response = await SubmissionProducer(submission);
-        return {queueResponse: response, submission};
+  async addSubmission(submissionPayload) {
+    const submission = await this.submissionRepository.createSubmission(
+      submissionPayload
+    );
+    if (!submission) {
+      // TODO: Add error handling here
+      throw new SubmissionCreationError(
+        "Failed to create a submission in the repository"
+      );
     }
+    console.log(submission);
+    const response = await SubmissionProducer(submission);
+    return { queueResponse: response, submission };
+  }
 }
 
-module.exports = SubmissionService
+module.exports = SubmissionService;
